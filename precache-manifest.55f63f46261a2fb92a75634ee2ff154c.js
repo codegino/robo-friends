ac15debc311bb8d370fae26ef15c0fdac1674cad
AnneMariel95 <77142075+AnneMariel95@users.noreply.github.com>
2021-02-16 16:22:53 +0800

    Add files via upload

---
+++ precache-manifest.55f63f46261a2fb92a75634ee2ff154c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd3a72ac6815438ce2708a51b41e85d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08b6b7a784e0045b8007",
+    "url": "/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "3f73f1d8e85184b35aa9",
+    "url": "/static/css/main.4d6722d4.chunk.css"
+  },
+  {
+    "revision": "08b6b7a784e0045b8007",
+    "url": "/static/js/2.55c9ea31.chunk.js"
+  },
+  {
+    "revision": "08aee544f36ba213a8865d2a35c6879d",
+    "url": "/static/js/2.55c9ea31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f73f1d8e85184b35aa9",
+    "url": "/static/js/main.e366d895.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
